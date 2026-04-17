b78826614c7102befe1ae996424b9ec254fc6629
sandropark <birdob@gmail.com>
2026-04-17 20:53:54 +0900

    init

---
+++ src/main/kotlin/com/sandro/asterumscheduler/AsterumSchedulerApplication.kt
@@ -0,0 +1,11 @@
+package com.sandro.asterumscheduler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsterumSchedulerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AsterumSchedulerApplication>(*args)
+}
---
+++ src/test/kotlin/com/sandro/asterumscheduler/AsterumSchedulerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sandro.asterumscheduler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsterumSchedulerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
