59099788995e173b3b9ca0129a01764731c727a4
sandropark <birdob@gmail.com>
2026-04-21 16:10:14 +0900

    refactor: 장소 조회 관련 코드 및 테스트 제거
    - LocationController, LocationService, 관련 테스트 및 유효성 검사 요청 클래스 삭제
    - 더 이상 사용되지 않는 EventReader 인터페이스 제거
    - 관련 잔여 설정 및 주석 정리

--- common/src/main/kotlin/com/sandro/asterumscheduler/common/event/EventReader.kt
+++
@@ -1,7 +0,0 @@
-package com.sandro.asterumscheduler.common.event
-
-import java.time.LocalDateTime
-
-interface EventReader {
-    fun findReservedLocationIds(start: LocalDateTime, end: LocalDateTime): List<Long>
-}
--- location/src/test/kotlin/com/sandro/asterumscheduler/location/TestLocationApplication.kt
+++
@@ -1,6 +0,0 @@
-package com.sandro.asterumscheduler.location
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class TestLocationApplication
